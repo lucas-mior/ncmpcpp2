2b7bccd7d3ef48edd0cde14135c9086c97d76fc0
lucas-mior <lucas.mior@tutamail.com>
2026-06-27 17:08:05 -0300

    cleanup

--- build.sh
+++ build.sh
@@ -6,10 +6,10 @@
 
 # autoreconf -fiv  # generate the `configure` script.
 
-./configure \
-    --enable-outputs \
-    --enable-visualizer \
-    --with-fftw \
-    --with-taglib
+# ./configure \
+#     --enable-outputs \
+#     --enable-visualizer \
+#     --with-fftw \
+#     --with-taglib
 
 make -j$(nproc)
