f4edc3148ce44ffa3c81eb000556fba9361c14eb
lucas-mior <lucas.mior@tutamail.com>
2026-07-01 21:10:19 -0300

    add style files

---
+++ style.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+# shellcheck disable=SC2035 
+
+# clang-format -i "$@"
+#     --style=file:/home/lucas/.config/clangd/clang-format.yaml
+
+for f in src/*.c src/*.h; do 
+    sed -E -i -f style.sed "$f"
+done
+
+for f in src/*.c src/*.h; do
+    sed -E -i 's/(\S+) \* (\S+)/\1*\2/g;' "$f"
+done
