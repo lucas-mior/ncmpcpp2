b29cbdfb5f4c98ea88c53c6e2a6b347d1b25aab7
lucas-mior <lucas.mior@tutamail.com>
2026-07-03 23:20:19 -0300

    more C

--- src/c/ncm_type_conversions.c
+++ src/c/ncm_type_conversions.c
@@ -93,22 +93,7 @@ ncm_tag_type_name(enum mpd_tag_type tag) {
 
 bool
 ncm_char_is_tag_type(char c) {
-    switch (c) {
-    case 'a':
-    case 'A':
-    case 't':
-    case 'b':
-    case 'y':
-    case 'n':
-    case 'g':
-    case 'c':
-    case 'p':
-    case 'd':
-    case 'C':
-        return true;
-    default:
-        return false;
-    }
+    return ncm_char_to_tag_type(c) != MPD_TAG_UNKNOWN;
 }
 
 enum mpd_tag_type
@@ -137,7 +122,109 @@ ncm_char_to_tag_type(char c) {
     case 'C':
         return MPD_TAG_COMMENT;
     default:
+        return MPD_TAG_UNKNOWN;
+    }
+}
+
+enum NcmSongGetter
+ncm_song_getter_from_char(char c) {
+    switch (c) {
+    case 'l':
+        return NCM_SONG_GETTER_LENGTH;
+    case 'D':
+        return NCM_SONG_GETTER_DIRECTORY;
+    case 'f':
+        return NCM_SONG_GETTER_NAME;
+    case 'F':
+        return NCM_SONG_GETTER_URI;
+    case 'a':
+        return NCM_SONG_GETTER_ARTIST;
+    case 'A':
+        return NCM_SONG_GETTER_ALBUM_ARTIST;
+    case 't':
+        return NCM_SONG_GETTER_TITLE;
+    case 'b':
+        return NCM_SONG_GETTER_ALBUM;
+    case 'y':
+        return NCM_SONG_GETTER_DATE;
+    case 'n':
+        return NCM_SONG_GETTER_TRACK_NUMBER;
+    case 'N':
+        return NCM_SONG_GETTER_TRACK;
+    case 'g':
+        return NCM_SONG_GETTER_GENRE;
+    case 'c':
+        return NCM_SONG_GETTER_COMPOSER;
+    case 'p':
+        return NCM_SONG_GETTER_PERFORMER;
+    case 'd':
+        return NCM_SONG_GETTER_DISC;
+    case 'C':
+        return NCM_SONG_GETTER_COMMENT;
+    case 'P':
+        return NCM_SONG_GETTER_PRIORITY;
+    default:
+        return NCM_SONG_GETTER_NONE;
+    }
+}
+
+enum NcmSongGetter
+ncm_song_getter_from_tag_type(enum mpd_tag_type tag) {
+    switch (tag) {
+    case MPD_TAG_ARTIST:
+        return NCM_SONG_GETTER_ARTIST;
+    case MPD_TAG_ALBUM:
+        return NCM_SONG_GETTER_ALBUM;
+    case MPD_TAG_ALBUM_ARTIST:
+        return NCM_SONG_GETTER_ALBUM_ARTIST;
+    case MPD_TAG_TITLE:
+        return NCM_SONG_GETTER_TITLE;
+    case MPD_TAG_TRACK:
+        return NCM_SONG_GETTER_TRACK;
+    case MPD_TAG_GENRE:
+        return NCM_SONG_GETTER_GENRE;
+    case MPD_TAG_DATE:
+        return NCM_SONG_GETTER_DATE;
+    case MPD_TAG_COMPOSER:
+        return NCM_SONG_GETTER_COMPOSER;
+    case MPD_TAG_PERFORMER:
+        return NCM_SONG_GETTER_PERFORMER;
+    case MPD_TAG_COMMENT:
+        return NCM_SONG_GETTER_COMMENT;
+    case MPD_TAG_DISC:
+        return NCM_SONG_GETTER_DISC;
+    default:
+        return NCM_SONG_GETTER_NONE;
+    }
+}
+
+enum mpd_tag_type
+ncm_song_getter_to_tag_type(enum NcmSongGetter getter) {
+    switch (getter) {
+    case NCM_SONG_GETTER_ARTIST:
         return MPD_TAG_ARTIST;
+    case NCM_SONG_GETTER_TITLE:
+        return MPD_TAG_TITLE;
+    case NCM_SONG_GETTER_ALBUM:
+        return MPD_TAG_ALBUM;
+    case NCM_SONG_GETTER_ALBUM_ARTIST:
+        return MPD_TAG_ALBUM_ARTIST;
+    case NCM_SONG_GETTER_TRACK:
+        return MPD_TAG_TRACK;
+    case NCM_SONG_GETTER_DATE:
+        return MPD_TAG_DATE;
+    case NCM_SONG_GETTER_GENRE:
+        return MPD_TAG_GENRE;
+    case NCM_SONG_GETTER_COMPOSER:
+        return MPD_TAG_COMPOSER;
+    case NCM_SONG_GETTER_PERFORMER:
+        return MPD_TAG_PERFORMER;
+    case NCM_SONG_GETTER_COMMENT:
+        return MPD_TAG_COMMENT;
+    case NCM_SONG_GETTER_DISC:
+        return MPD_TAG_DISC;
+    default:
+        return MPD_TAG_UNKNOWN;
     }
 }
 
