11a0b2d9c4aa04fdec9a11894b5437a9a3d0c11a
lucas-mior <lucas.mior@tutamail.com>
2026-07-04 19:24:26 -0300

    more C

---
+++ src/screens/nc_song_info.h
@@ -0,0 +1,36 @@
+#if !defined(NCMPCPP_NC_SONG_INFO_H)
+#define NCMPCPP_NC_SONG_INFO_H
+
+#include "screens/nc_screen.h"
+
+#if defined(__cplusplus)
+extern "C" {
+#endif
+
+typedef struct NcSongInfoScreen {
+    NcScreen base;
+
+    int64 start_x;
+    int64 start_y;
+    int64 width;
+    int64 height;
+} NcSongInfoScreen;
+
+void nc_song_info_screen_init(NcSongInfoScreen *screen,
+                              NcScreenCallbacks callbacks, void *user,
+                              int64 start_x, int64 width,
+                              int64 main_start_y, int64 main_height);
+void nc_song_info_screen_set_geometry(NcSongInfoScreen *screen,
+                                      int64 start_x, int64 width,
+                                      int64 main_start_y,
+                                      int64 main_height);
+int64 nc_song_info_screen_start_x(NcSongInfoScreen *screen);
+int64 nc_song_info_screen_start_y(NcSongInfoScreen *screen);
+int64 nc_song_info_screen_width(NcSongInfoScreen *screen);
+int64 nc_song_info_screen_height(NcSongInfoScreen *screen);
+
+#if defined(__cplusplus)
+}
+#endif
+
+#endif /* NCMPCPP_NC_SONG_INFO_H */
