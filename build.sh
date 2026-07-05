61e401ead53d846f784422dc6c9a8836e7fae562
lucas-mior <lucas.mior@tutamail.com>
2026-07-05 14:29:47 -0300

    wip

--- build.sh
+++ build.sh
@@ -30,19 +30,9 @@ make clean
 
 autoreconf -fiv  # generate the `configure` script.
 
-# ./configure \
-#     --enable-outputs \
-#     --enable-visualizer \
-#     --with-fftw \
-#     --with-taglib
-
 ./configure \
   --disable-dependency-tracking \
   --with-lto=no \
-  --enable-outputs \
-  --enable-visualizer \
-  --with-fftw \
-  --with-taglib \
   CC="$CC" \
   CXX="$CXX" \
   CFLAGS="$CFLAGS" \
