7d12b9065a7e2d6f167a4048ca73e53ad507c2e1
lucas-mior <lucas.mior@tutamail.com>
2026-07-06 22:32:51 -0300

    more C

--- src/c/ncm_mpd_item.h
+++ src/c/ncm_mpd_item.h
@@ -2,19 +2,13 @@
 #define NCM_MPD_ITEM_H
 
 #include "c/ncm_defs.h"
-
-#if defined(__cplusplus)
-#include <new>
-#endif
 #include "c/ncm_directory.h"
 #include "c/ncm_playlist.h"
 #include "c/ncm_song.h"
 
 struct mpd_entity;
 
-#if defined(__cplusplus)
-extern "C" {
-#endif
+NCM_EXTERN_C_BEGIN
 
 enum NcmMpdItemKind {
     NCM_MPD_ITEM_UNKNOWN,
@@ -27,26 +21,12 @@ typedef union NcmMpdItemValue {
     NcmSong song;
     NcmDirectory directory;
     NcmPlaylist playlist;
-
-#if defined(__cplusplus)
-    NcmMpdItemValue() { }
-    ~NcmMpdItemValue() { }
-#endif
 } NcmMpdItemValue;
 
 
 typedef struct NcmMpdItem {
     enum NcmMpdItemKind kind;
     NcmMpdItemValue value;
-
-#if defined(__cplusplus)
-    NcmMpdItem();
-    NcmMpdItem(const NcmMpdItem &rhs);
-    NcmMpdItem(NcmMpdItem &&rhs) noexcept;
-    NcmMpdItem &operator=(const NcmMpdItem &rhs);
-    NcmMpdItem &operator=(NcmMpdItem &&rhs) noexcept;
-    ~NcmMpdItem();
-#endif
 } NcmMpdItem;
 
 void ncm_mpd_item_init(NcmMpdItem *item);
@@ -71,56 +51,6 @@ NcmSong *ncm_mpd_item_song(NcmMpdItem *item);
 NcmDirectory *ncm_mpd_item_directory(NcmMpdItem *item);
 NcmPlaylist *ncm_mpd_item_playlist(NcmMpdItem *item);
 
-#if defined(__cplusplus)
-}
-#endif
-
-#if defined(__cplusplus)
-inline NcmMpdItem::NcmMpdItem()
-{
-    ncm_mpd_item_init(this);
-}
-
-inline NcmMpdItem::NcmMpdItem(const NcmMpdItem &rhs)
-{
-    ncm_mpd_item_init(this);
-    if (!ncm_mpd_item_copy(this, const_cast<NcmMpdItem *>(&rhs)))
-    {
-        throw std::bad_alloc();
-    }
-}
-
-inline NcmMpdItem::NcmMpdItem(NcmMpdItem &&rhs) noexcept
-{
-    ncm_mpd_item_init(this);
-    ncm_mpd_item_move(this, &rhs);
-}
-
-inline NcmMpdItem &NcmMpdItem::operator=(const NcmMpdItem &rhs)
-{
-    if (this != &rhs)
-    {
-        if (!ncm_mpd_item_copy(this, const_cast<NcmMpdItem *>(&rhs)))
-        {
-            throw std::bad_alloc();
-        }
-    }
-    return *this;
-}
-
-inline NcmMpdItem &NcmMpdItem::operator=(NcmMpdItem &&rhs) noexcept
-{
-    if (this != &rhs)
-    {
-        ncm_mpd_item_move(this, &rhs);
-    }
-    return *this;
-}
-
-inline NcmMpdItem::~NcmMpdItem()
-{
-    ncm_mpd_item_destroy(this);
-}
-#endif
+NCM_EXTERN_C_END
 
 #endif /* NCM_MPD_ITEM_H */
