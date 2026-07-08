3d8e33cc7467b30b131cb82343e3805afadc4142
lucas-mior <lucas.mior@tutamail.com>
2026-07-08 17:51:39 -0300

    more C

--- src/ui_state.h
+++ src/ui_state.h
@@ -1,6 +1,8 @@
 #if !defined(NCMPCPP_UI_STATE_H)
 #define NCMPCPP_UI_STATE_H
 
+#include <stdbool.h>
+
 #include "cbase/primitives.h"
 #include "curses/nc_window.h"
 
@@ -24,6 +26,14 @@ void ui_state_set_main_start_y(int64 value);
 int64 ui_state_main_start_y(void);
 void ui_state_set_main_height(int64 value);
 int64 ui_state_main_height(void);
+void ui_state_set_header_height(int64 value);
+int64 ui_state_header_height(void);
+void ui_state_set_footer_height(int64 value);
+int64 ui_state_footer_height(void);
+void ui_state_set_footer_start_y(int64 value);
+int64 ui_state_footer_start_y(void);
+void ui_state_set_statusbar_visibility_baseline(bool value);
+bool ui_state_statusbar_visibility_baseline(void);
 
 #if defined(__cplusplus)
 }
