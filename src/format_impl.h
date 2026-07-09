f7aad6ac20c171afd209152447dfa108a16908c3
lucas-mior <lucas.mior@tutamail.com>
2026-07-09 06:34:41 -0300

    cleanup

--- src/format_impl.h
+++ src/format_impl.h
@@ -1,13 +1,3 @@
-/***************************************************************************
- *   Copyright (C) 2008-2021 by Andrzej Rybczak                            *
- *   andrzej@rybczak.net                                                   *
- *                                                                         *
- *   This program is free software; you can redistribute it and/or modify  *
- *   it under the terms of the GNU General Public License as published by  *
- *   the Free Software Foundation; either version 2 of the License, or     *
- *   (at your option) any later version.                                   *
- ***************************************************************************/
-
 #ifndef NCMPCPP_HAVE_FORMAT_IMPL_H
 #define NCMPCPP_HAVE_FORMAT_IMPL_H
 
