e0364a0808eef2c9b0ff4d38116253cac65c9703
lucas-mior <lucas.mior@tutamail.com>
2026-07-14 21:40:58 -0300

    delete unused C++ headers

--- src/enums.h
+++
@@ -1,6 +0,0 @@
-#if !defined(NCMPCPP_ENUMS_H)
-#define NCMPCPP_ENUMS_H
-
-#include "c/ncm_enums.h"
-
-#endif /* NCMPCPP_ENUMS_H */
--- src/screens/server_info.h
+++
@@ -1,6 +0,0 @@
-#ifndef NCMPCPP_SERVER_INFO_H
-#define NCMPCPP_SERVER_INFO_H
-
-#include "screens/native_c_screens.h"
-
-#endif /* NCMPCPP_SERVER_INFO_H */
