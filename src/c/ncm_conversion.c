6c392794616c31d925915e72f6ef2d9200949d22
lucas-mior <lucas.mior@tutamail.com>
2026-07-20 18:06:18 -0300

    style (whitespace)

--- src/c/ncm_conversion.c
+++ src/c/ncm_conversion.c
@@ -178,8 +178,7 @@ ncm_parse_int32(char *source, int32 source_len, int32 *out, NcmError *error) {
 }
 
 bool
-ncm_parse_double(char *source, int32 source_len,
-                 double *out, NcmError *error) {
+ncm_parse_double(char *source, int32 source_len, double *out, NcmError *error) {
     NcmBuffer buffer;
     char *end;
     double value;
