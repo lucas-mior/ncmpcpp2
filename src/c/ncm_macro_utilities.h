7362435808e297df5e44f50bbcc22d64335be5e3
lucas-mior <lucas.mior@tutamail.com>
2026-07-26 23:55:18 -0300

    cleanup

--- src/c/ncm_macro_utilities.h
+++ src/c/ncm_macro_utilities.h
@@ -1,10 +1,9 @@
 #if !defined(NCM_MACRO_UTILITIES_H)
 #define NCM_MACRO_UTILITIES_H
 
-#include <stdbool.h>
+#include "cbase.h"
 
 #include "c/ncm_error.h"
-#include "cbase/primitives.h"
 
 bool ncm_macro_run_external_command(char *command, int32 command_len,
                                     bool block, NcmError *error);
