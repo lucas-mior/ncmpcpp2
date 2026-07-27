7693f3ec03c8e6a569f0976d6e53e7e7fd16efc1
lucas-mior <lucas.mior@tutamail.com>
2026-07-27 00:01:19 -0300

    wip

--- src/c/ncm_comparators.h
+++ src/c/ncm_comparators.h
@@ -1,9 +1,7 @@
 #if !defined(NCM_COMPARATORS_H)
 #define NCM_COMPARATORS_H
 
-#include <stdbool.h>
-
-#include "cbase/primitives.h"
+#include "cbase.h"
 
 int32 ncm_compare_locale_strings(char *left, int32 left_len,
                                  char *right, int32 right_len,
--- src/screens/nc_browser.h
+++ src/screens/nc_browser.h
@@ -1,7 +1,7 @@
 #if !defined(NCMPCPP_NC_BROWSER_H)
 #define NCMPCPP_NC_BROWSER_H
 
-#include <stdbool.h>
+#include "cbase.h"
 
 #include "c/ncm_app_arrays.h"
 #include "c/ncm_enums.h"
