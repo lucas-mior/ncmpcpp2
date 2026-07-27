81232a7b2f65a9058708ecbdd46e27efd1abbc42
lucas-mior <lucas.mior@tutamail.com>
2026-07-27 00:38:57 -0300

    style (whitespace)

--- src/c/ncm_string.h
+++ src/c/ncm_string.h
@@ -14,10 +14,10 @@ void ncm_string_lowercase_ascii(char *string, int32 string_len);
 int32 ncm_string_find_char(char *string, int32 string_len, char needle);
 bool ncm_string_contains_char(char *string, int32 string_len, char needle);
 StrBuilder ncm_string_shared_directory(char *left, int32 left_len,
-                                      char *right, int32 right_len);
+                                       char *right, int32 right_len);
 StrBuilder ncm_string_get_enclosed(char *string, int32 string_len,
-                                  char open, char close,
-                                  int32 start, int32 *pos);
+                                   char open, char close,
+                                   int32 start, int32 *pos);
 void ncm_string_remove_chars(char *string, int32 *string_len,
                              char *chars, int32 chars_len);
 void ncm_string_remove_invalid_filename_chars(char *filename,
