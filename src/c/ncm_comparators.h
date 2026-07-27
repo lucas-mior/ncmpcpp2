f0b3a8de9d99703279d620786c851fd73d9aeaf2
lucas-mior <lucas.mior@tutamail.com>
2026-07-26 22:30:09 -0300

    style (whitespace)

--- src/c/ncm_comparators.h
+++ src/c/ncm_comparators.h
@@ -6,7 +6,7 @@
 #include "cbase/primitives.h"
 
 int32 ncm_compare_locale_strings(char *left, int32 left_len,
-                                  char *right, int32 right_len,
-                                  bool ignore_the);
+                                 char *right, int32 right_len,
+                                 bool ignore_the);
 
 #endif /* NCM_COMPARATORS_H */
