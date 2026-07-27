0ee502420d9a65604c22401f0a61333df6b10459
lucas-mior <lucas.mior@tutamail.com>
2026-07-26 23:53:18 -0300

    cleanup

--- src/screens/nc_playlist_editor.h
+++ src/screens/nc_playlist_editor.h
@@ -1,7 +1,7 @@
 #if !defined(NCMPCPP_NC_PLAYLIST_EDITOR_H)
 #define NCMPCPP_NC_PLAYLIST_EDITOR_H
 
-#include <stdbool.h>
+#include "cbase.h"
 
 #include "c/ncm_app_arrays.h"
 #include "c/ncm_mpd_client.h"
