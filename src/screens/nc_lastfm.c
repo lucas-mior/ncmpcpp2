b76f4eb3bbb85c76eb26e49bf5536f04306d25bb
lucas-mior <lucas.mior@tutamail.com>
2026-08-04 16:36:06 -0300

    update cbase and prompt 5 (compilation still fails)

--- src/screens/nc_lastfm.c
+++ src/screens/nc_lastfm.c
@@ -28,17 +28,12 @@ typedef struct NativeLastfmFindState {
     NcBuffer *buffer;
 } NativeLastfmFindState;
 
-static NcWindow *lastfm_active_window_callback(NcScreen *screen);
-static void lastfm_refresh_callback(NcScreen *screen);
-static void lastfm_refresh_window_callback(NcScreen *screen);
-static void lastfm_scroll_callback(NcScreen *screen, enum NcScroll where);
 static void lastfm_switch_to_callback(NcScreen *screen);
 static void lastfm_resize_callback(NcScreen *screen);
 static char *lastfm_title_callback(NcScreen *screen);
 static void lastfm_update_callback(NcScreen *screen);
 static void lastfm_mouse_button_pressed_callback(NcScreen *screen,
                                                  MEVENT event);
-static void lastfm_destroy_callback(NcScreen *screen);
 static bool native_lastfm_set_title(NativeLastfmScreen *screen,
                                     char *title, int32 title_len);
 static NativeLastfmJob *native_lastfm_job_create(
@@ -65,6 +60,25 @@ static void native_lastfm_mouse_scroll(NativeLastfmScreen *screen,
                                        enum NcScroll where);
 static void native_lastfm_display(NativeLastfmScreen *screen);
 static void native_lastfm_flush(NativeLastfmScreen *screen);
+
+#define NC_SCREEN_IMPL_TYPE NativeLastfmScreen
+#define NC_SCREEN_IMPL_PREFIX lastfm
+#define NC_SCREEN_IMPL_PUBLIC_PREFIX native_lastfm_screen
+#define NC_SCREEN_IMPL_BASE_FIELD screen
+#define NC_SCREEN_IMPL_SCROLLPAD_BASE screen.scrollpad_screen
+#define NC_SCREEN_IMPL_NO_GEOMETRY_ACCESSORS
+#define NC_SCREEN_IMPL_WINDOW_FIELD window
+#define NC_SCREEN_IMPL_SCROLLPAD_FIELD scrollpad
+#define NC_SCREEN_IMPL_REFRESH_CALLBACK native_lastfm_display
+#define NC_SCREEN_IMPL_SWITCH_TO_CALLBACK lastfm_switch_to_callback
+#define NC_SCREEN_IMPL_RESIZE_CALLBACK lastfm_resize_callback
+#define NC_SCREEN_IMPL_TITLE_CALLBACK lastfm_title_callback
+#define NC_SCREEN_IMPL_UPDATE_CALLBACK lastfm_update_callback
+#define NC_SCREEN_IMPL_MOUSE_CALLBACK lastfm_mouse_button_pressed_callback
+#define NC_SCREEN_IMPL_DESTROY_TYPED_CALLBACK native_lastfm_screen_destroy
+#define NC_SCREEN_IMPL_LOCKABLE true
+#define NC_SCREEN_IMPL_MERGABLE true
+#include "screens/nc_screen_impl_template.c"
 
 void
 nc_lastfm_screen_init(NcLastfmScreen *screen,
@@ -117,26 +131,6 @@ nc_lastfm_screen_height(NcLastfmScreen *screen) {
     return nc_scrollpad_screen_height(&screen->scrollpad_screen);
 }
 
-static NcScreenOps
-native_lastfm_callbacks(void) {
-    NcScreenOps callbacks = {0};
-
-    callbacks.active_window = lastfm_active_window_callback;
-    callbacks.refresh = lastfm_refresh_callback;
-    callbacks.refresh_window = lastfm_refresh_window_callback;
-    callbacks.scroll = lastfm_scroll_callback;
-    callbacks.switch_to = lastfm_switch_to_callback;
-    callbacks.resize = lastfm_resize_callback;
-    callbacks.title = lastfm_title_callback;
-    callbacks.update = lastfm_update_callback;
-    callbacks.mouse_button_pressed = lastfm_mouse_button_pressed_callback;
-    callbacks.lockable = true;
-    callbacks.mergable = true;
-    callbacks.destroy = lastfm_destroy_callback;
-
-    return callbacks;
-}
-
 void
 native_lastfm_screen_init(NativeLastfmScreen *screen,
                           int32 start_x, int32 width,
@@ -144,7 +138,7 @@ native_lastfm_screen_init(NativeLastfmScreen *screen,
                           NcColor color, NcBorder border,
                           int32 lines_scrolled) {
     nc_lastfm_screen_init(&screen->screen,
-                          native_lastfm_callbacks(),
+                          lastfm_ops,
                           screen,
                           start_x, width,
                           main_start_y, main_height);
@@ -204,11 +198,6 @@ native_lastfm_screen_destroy(NativeLastfmScreen *screen) {
     screen->initialized = false;
 
     return;
-}
-
-NcScreen *
-native_lastfm_screen_base(NativeLastfmScreen *screen) {
-    return nc_lastfm_screen_base(&screen->screen);
 }
 
 NcWindow *
@@ -405,37 +394,6 @@ native_lastfm_screen_find(NativeLastfmScreen *screen,
     return result;
 }
 
-static NativeLastfmScreen *
-lastfm_from_screen(NcScreen *screen) {
-    return nc_screen_user(screen);
-}
-
-static NcWindow *
-lastfm_active_window_callback(NcScreen *screen) {
-    return native_lastfm_screen_window(lastfm_from_screen(screen));
-}
-
-static void
-lastfm_refresh_callback(NcScreen *screen) {
-    native_lastfm_display(lastfm_from_screen(screen));
-    return;
-}
-
-static void
-lastfm_refresh_window_callback(NcScreen *screen) {
-    native_lastfm_display(lastfm_from_screen(screen));
-    return;
-}
-
-static void
-lastfm_scroll_callback(NcScreen *screen, enum NcScroll where) {
-    NativeLastfmScreen *lastfm;
-
-    lastfm = lastfm_from_screen(screen);
-    nc_scrollpad_scroll(&lastfm->scrollpad, &lastfm->window, where);
-    return;
-}
-
 static void
 lastfm_switch_to_callback(NcScreen *screen) {
     ncm_title_draw_header(nc_screen_title(screen),
@@ -481,14 +439,6 @@ lastfm_mouse_button_pressed_callback(NcScreen *screen, MEVENT event) {
     } else if (event.bstate & BUTTON4_PRESSED) {
         native_lastfm_mouse_scroll(lastfm, NC_SCROLL_UP);
     }
-    return;
-}
-
-
-
-static void
-lastfm_destroy_callback(NcScreen *screen) {
-    native_lastfm_screen_destroy(lastfm_from_screen(screen));
     return;
 }
 
