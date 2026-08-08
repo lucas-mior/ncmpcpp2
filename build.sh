eeee0f2d553ed86b1b9895a498da077708708215
lucas-mior <lucas.mior@tutamail.com>
2026-08-08 12:06:16 -0300

    update cbase

--- build.sh
+++ build.sh
@@ -36,13 +36,13 @@ if ! command -v "$CC" > /dev/null 2>&1; then
     CC=cc
 fi
 
-CPPFLAGS="$CPPFLAGS -D_DEFAULT_SOURCE -D_XOPEN_SOURCE=700"
+CPPFLAGS="$CPPFLAGS -D_DEFAULT_SOURCE"
 
 CFLAGS="$CFLAGS -std=c11"
 CFLAGS="$CFLAGS -Wfatal-errors"
 CFLAGS="$CFLAGS -Wextra -Wall"
 CFLAGS="$CFLAGS -Werror=all -Werror=extra"
-# CFLAGS="$CFLAGS -Werror"  # Only uncomment occasionally, keep this line
+CFLAGS="$CFLAGS -Werror"  # Only uncomment occasionally, keep this line
 CFLAGS="$CFLAGS -Wno-cast-qual"
 CFLAGS="$CFLAGS -Wno-constant-logical-operand"
 CFLAGS="$CFLAGS -Wno-float-equal"
