1b90c67711c8d121dfb6e830b2f1654add502205
lucas-mior <lucas.mior@tutamail.com>
2026-08-10 23:13:58 -0300

    wip

--- cbase/memory.c
+++ cbase/memory.c
@@ -78,8 +78,9 @@ allocations_unlock(void) {
 #define X64(FUNC)                                                 \
 CBASE_API_DEF void                                                \
 CAT(FUNC, 64)(void *dest, void *source, int64 n) {                \
-    if (n == 0)                                                   \
+    if (n == 0) {                                                 \
         return;                                                   \
+    }                                                             \
     if (DEBUGGING) {                                              \
         ASSERT(dest != NULL);                                     \
         ASSERT(source != NULL);                                   \
