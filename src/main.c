f802bfe9cfc7c6da9a57ca635c9bf95a32e11596
lucas-mior <lucas.mior@tutamail.com>
2026-08-11 16:38:51 -0300

    remove _legacy_ from identifiers

--- src/main.c
+++ src/main.c
@@ -164,16 +164,16 @@ app_at_exit(void) {
     ncm_window_title_set_cstring("");
 
     if (app_header_window) {
-        ncmpcpp_legacy_window_destroy(app_header_window);
+        ncmpcpp_window_destroy(app_header_window);
         app_header_window = NULL;
         ui_state_set_header_window(NULL);
     }
     if (app_footer_window) {
-        ncmpcpp_legacy_window_destroy(app_footer_window);
+        ncmpcpp_window_destroy(app_footer_window);
         app_footer_window = NULL;
         ui_state_set_footer_window(NULL);
     }
-    ncmpcpp_legacy_destroy_screen();
+    ncmpcpp_destroy_screen();
 
     app_restore_stderr();
     app_destroy_state();
@@ -182,37 +182,37 @@ app_at_exit(void) {
 
 static void
 app_create_windows(void) {
-    ncmpcpp_legacy_set_statusbar_visibility_baseline(
+    ncmpcpp_set_statusbar_visibility_baseline(
         Config.statusbar_visibility);
 
     if (Config.design == NCM_DESIGN_ALTERNATIVE) {
         Config.statusbar_visibility = false;
     }
 
-    ncmpcpp_legacy_set_windows_dimensions();
-    ncmpcpp_legacy_initialize_screens();
+    ncmpcpp_set_windows_dimensions();
+    ncmpcpp_initialize_screens();
 
-    app_header_window = ncmpcpp_legacy_window_create(
-        0, 0, COLS, ncmpcpp_legacy_header_height(), Config.header_color);
+    app_header_window = ncmpcpp_window_create(
+        0, 0, COLS, ncmpcpp_header_height(), Config.header_color);
     ui_state_set_header_window(app_header_window);
     if (Config.header_visibility || (Config.design == NCM_DESIGN_ALTERNATIVE)) {
-        ncmpcpp_legacy_window_display(app_header_window);
+        ncmpcpp_window_display(app_header_window);
     }
 
-    app_footer_window = ncmpcpp_legacy_window_create(
-        0, ncmpcpp_legacy_footer_start_y(), COLS,
-        ncmpcpp_legacy_footer_height(), Config.statusbar_color);
+    app_footer_window = ncmpcpp_window_create(
+        0, ncmpcpp_footer_start_y(), COLS,
+        ncmpcpp_footer_height(), Config.statusbar_color);
     ui_state_set_footer_window(app_footer_window);
     return;
 }
 
 static void
 app_apply_startup_screen(void) {
-    ncmpcpp_legacy_playlist_switch_to();
+    ncmpcpp_playlist_switch_to();
 
-    if (Config.startup_screen_type != ncmpcpp_legacy_current_screen_type()) {
+    if (Config.startup_screen_type != ncmpcpp_current_screen_type()) {
         assert(
-            ncmpcpp_legacy_switch_to_screen_type(Config.startup_screen_type));
+            ncmpcpp_switch_to_screen_type(Config.startup_screen_type));
     }
 
     if (Config.has_startup_slave_screen_type) {
@@ -220,12 +220,12 @@ app_apply_startup_screen(void) {
         enum ScreenType slave_screen_type;
 
         slave_screen_type = Config.startup_slave_screen_type;
-        screen_locked = ncmpcpp_legacy_lock_current_screen();
+        screen_locked = ncmpcpp_lock_current_screen();
         if (screen_locked
-            && (slave_screen_type != ncmpcpp_legacy_current_screen_type())) {
-            assert(ncmpcpp_legacy_switch_to_screen_type(slave_screen_type));
+            && (slave_screen_type != ncmpcpp_current_screen_type())) {
+            assert(ncmpcpp_switch_to_screen_type(slave_screen_type));
             if (!Config.startup_slave_screen_focus) {
-                ncmpcpp_legacy_execute_action(NCM_ACTION_MASTER_SCREEN);
+                ncmpcpp_execute_action(NCM_ACTION_MASTER_SCREEN);
             }
         }
     }
@@ -234,12 +234,12 @@ app_apply_startup_screen(void) {
 
 static void
 app_connect_if_due(NcmTimePoint *connect_attempt) {
-    if (!ncmpcpp_legacy_mpd_connected()
+    if (!ncmpcpp_mpd_connected()
         && (global_timer_elapsed_ms(*connect_attempt) > 1000)) {
         *connect_attempt = global_timer;
-        ncmpcpp_legacy_status_clear();
+        ncmpcpp_status_clear();
         nc_window_clear_fd_callbacks(app_footer_window);
-        ncmpcpp_legacy_connect_or_report();
+        ncmpcpp_connect_or_report();
     }
     return;
 }
@@ -252,7 +252,7 @@ app_execute_key(NcKey input) {
     bindings = ncm_bindings_configuration_get(&Bindings, input);
     executed = false;
     for (int32 i = 0; i < bindings.len; i += 1) {
-        if (ncmpcpp_legacy_execute_binding(bindings.data + i)) {
+        if (ncmpcpp_execute_binding(bindings.data + i)) {
             executed = true;
             break;
         }
@@ -264,7 +264,7 @@ app_execute_key(NcKey input) {
 
 static bool
 app_exit_requested(void) {
-    return ncmpcpp_legacy_exit_requested()
+    return ncmpcpp_exit_requested()
            || ncm_action_runtime_exit_requested(NULL);
 }
 
@@ -292,8 +292,8 @@ main(int32 argc, char **argv) {
     signal(SIGPIPE, SIG_IGN);
     signal(SIGWINCH, app_signal_handler);
 
-    ncmpcpp_legacy_set_noidle_status_callback();
-    ncmpcpp_legacy_init_screen(Config.colors_enabled, Config.mouse_support);
+    ncmpcpp_set_noidle_status_callback();
+    ncmpcpp_init_screen(Config.colors_enabled, Config.mouse_support);
     app_create_windows();
 
     global_timer_update(NULL);
@@ -307,11 +307,11 @@ main(int32 argc, char **argv) {
         app_connect_if_due(&connect_attempt);
 
         if (app_resize_requested) {
-            ncmpcpp_legacy_resize_screen(true);
+            ncmpcpp_resize_screen(true);
             app_resize_requested = 0;
         }
 
-        ncmpcpp_legacy_update_environment(!key_pressed, key_pressed, false);
+        ncmpcpp_update_environment(!key_pressed, key_pressed, false);
 
         input = ncm_read_key(app_footer_window);
         key_pressed = input != NC_KEY_NONE;
@@ -321,7 +321,7 @@ main(int32 argc, char **argv) {
 
         global_timer_update(NULL);
         app_execute_key(input);
-        ncmpcpp_legacy_playlist_enable_highlighting_if_current();
+        ncmpcpp_playlist_enable_highlighting_if_current();
     }
 
     exit(EXIT_SUCCESS);
