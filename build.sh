0100da32024ddce349dc565de123b0f4e07850c8
lucas-mior <lucas.mior@tutamail.com>
2026-08-12 16:00:09 -0300

    errors

--- build.sh
+++ build.sh
@@ -27,7 +27,7 @@ CPPFLAGS="$CPPFLAGS -I$dir -I$dir/src -I$dir/cbase"
 CFLAGS="$CFLAGS -std=c11"
 CFLAGS="$CFLAGS -Wfatal-errors"
 CFLAGS="$CFLAGS -Wextra -Wall"
-# CFLAGS="$CFLAGS -Werror=all -Werror=extra"
+CFLAGS="$CFLAGS -Werror=all -Werror=extra"
 # CFLAGS="$CFLAGS -Werror"  # Only uncomment occasionally, keep this line
 
 if [ "$CC" = "clang" ] || [ "$CC" = "zig cc" ]; then
