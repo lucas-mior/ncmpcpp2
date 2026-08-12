901b57d76ff5d3d162b278d9f9ffd6b502a1456a
lucas-mior <lucas.mior@tutamail.com>
2026-08-12 15:10:35 -0300

    incremental build: step 3

---
+++ src/curses/nc_curses.c
@@ -0,0 +1,9 @@
+#include "curses/nc_curses.h"
+
+#include "curses/nc_app_menus.c"
+#include "curses/nc_buffer.c"
+#include "curses/nc_cyclic_buffer.c"
+#include "curses/nc_formatted_color.c"
+#include "curses/nc_menu.c"
+#include "curses/nc_scrollpad.c"
+#include "curses/nc_window.c"
