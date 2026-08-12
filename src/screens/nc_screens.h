7454bb2091720bf95ef5d507c76868a16d758d75
lucas-mior <lucas.mior@tutamail.com>
2026-08-12 15:07:56 -0300

    incremental build: step 2

---
+++ src/screens/nc_screens.h
@@ -0,0 +1,28 @@
+#if !defined(NCMPCPP_NC_SCREENS_H)
+#define NCMPCPP_NC_SCREENS_H
+
+#include "screens/app_screens.h"
+#include "screens/nc_browser.h"
+#include "screens/nc_help.h"
+#include "screens/nc_lastfm.h"
+#include "screens/nc_lyrics.h"
+#include "screens/nc_media_library.h"
+#include "screens/nc_outputs.h"
+#include "screens/nc_playlist.h"
+#include "screens/nc_playlist_editor.h"
+#include "screens/nc_screen.h"
+#include "screens/nc_scrollpad_screen.h"
+#include "screens/nc_search_engine.h"
+#include "screens/nc_sel_items_adder.h"
+#include "screens/nc_server_info.h"
+#include "screens/nc_song_info.h"
+#include "screens/nc_sort_playlist.h"
+#include "screens/nc_tag_editor.h"
+#include "screens/nc_tiny_tag_editor.h"
+#include "screens/nc_visualizer.h"
+#include "screens/screen_defs.h"
+#include "screens/screen_switcher.h"
+#include "screens/screen_type.h"
+#include "screens/song_info.h"
+
+#endif /* NCMPCPP_NC_SCREENS_H */
