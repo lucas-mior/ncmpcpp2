b3496a53b976e66b467f0f40c46b1c99c8d8673e
lucas-mior <lucas.mior@tutamail.com>
2026-08-12 15:07:56 -0300

    incremental build: step 2

---
+++ src/curses/nc_curses.h
@@ -0,0 +1,12 @@
+#if !defined(NCMPCPP_NC_CURSES_H)
+#define NCMPCPP_NC_CURSES_H
+
+#include "curses/nc_app_menus.h"
+#include "curses/nc_buffer.h"
+#include "curses/nc_cyclic_buffer.h"
+#include "curses/nc_formatted_color.h"
+#include "curses/nc_menu.h"
+#include "curses/nc_scrollpad.h"
+#include "curses/nc_window.h"
+
+#endif /* NCMPCPP_NC_CURSES_H */
