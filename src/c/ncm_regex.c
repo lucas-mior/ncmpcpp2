bcddcbd1a5a4a13763e6d7156e9ff62eba74cbdf
lucas-mior <lucas.mior@tutamail.com>
2026-08-18 12:47:21 -0300

    reject empty pattern

--- src/c/ncm_regex.c
+++ src/c/ncm_regex.c
@@ -129,7 +129,9 @@ ncm_regex_compile(NcmRegex *regex, char *pattern, int32 pattern_len,
 
     ncm_regex_destroy(regex);
 
-    if (flags & NCM_REGEX_LITERAL) {
+    if (pattern_len == 0) {
+        SB_APPEND(&compiled_pattern, STRLIT("^"));
+    } else if (flags & NCM_REGEX_LITERAL) {
         ncm_regex_escape_literal(&escaped, pattern, pattern_len);
         SB_APPEND(&compiled_pattern, escaped.data, escaped.len);
     } else {
