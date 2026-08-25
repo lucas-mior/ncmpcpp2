59b03706a99cbf5b375d1f4a880b6303e431efc1
lucas-mior <lucas.mior@tutamail.com>
2026-08-25 17:04:51 -0300

    wip

--- cbase/cbase.h
+++ cbase/cbase.h
@@ -246,7 +246,9 @@ strequal2(char *a, int32 a_len, char *b, int32 b_len) {
     if (a_len != b_len) {
         return false;
     }
-
+    if (a_len == 0) {
+        return true;
+    }
     if (DEBUGGING) {
         if ((a == NULL) || (b == NULL)) {
             TRAP();
