eb256c3d442c83b58a45130eae740446253dffa0
lucas-mior <lucas.mior@tutamail.com>
2026-09-02 12:29:16 -0300

    wip

--- build.sh
+++ build.sh
@@ -144,9 +144,7 @@ build|debug-fast|fast_feedback)
     load_package_flags
 
     trace_on
-    $CC $CPPFLAGS $PKG_CFLAGS $CFLAGS \
-        -o $exe src/main.c            \
-        $PKG_LIBS $LDFLAGS
+    $CC $CPPFLAGS $PKG_CFLAGS $CFLAGS -o $exe src/main.c $PKG_LIBS $LDFLAGS
     trace_off
     ;;
 check)
