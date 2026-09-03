c8165d37e4f3bc861ca839eaa6f7c9046f5250b5
lucas-mior <lucas.mior@tutamail.com>
2026-09-03 17:59:10 -0300

    wip

--- src/screens/nc_playlist.c
+++ src/screens/nc_playlist.c
@@ -1218,17 +1218,11 @@ playlist_song_is_now_playing(NcmSong *song) {
 
 static void
 playlist_print_buffer(NcWindow *window, NcBuffer *buffer) {
-    NcBufferProperty *properties;
-    char *data;
-    int32 property_count;
-    int32 property_index;
-    int32 len;
-
-    data = nc_buffer_data(buffer);
-    len = nc_buffer_len(buffer);
-    properties = nc_buffer_properties(buffer);
-    property_count = nc_buffer_property_count(buffer);
-    property_index = 0;
+    NcBufferProperty *properties = nc_buffer_properties(buffer);
+    char *data = nc_buffer_data(buffer);
+    int32 len = nc_buffer_len(buffer);
+    int32 property_count = nc_buffer_property_count(buffer);
+    int32 property_index = 0;
 
     for (int32 i = 0;; i += 1) {
         while ((property_index < property_count)
