d882f516579eed30fa0054a4767f05108a80e480
lucas-mior <lucas.mior@tutamail.com>
2026-09-03 16:03:20 -0300

    wip

--- src/screens/nc_browser.c
+++ src/screens/nc_browser.c
@@ -21,16 +21,16 @@ static void browser_update(NcScreen *screen);
 static void browser_mouse_button_pressed(NcScreen *screen, MEVENT event);
 
 // declarations to delete
+static bool browser_local_path_has_supported_extension(BrowserScreen *, char *, int32 );
+static int32 browser_real_path(BrowserScreen *, NcmStringView, StrBuilder *, NcmError *);
+static int32 browser_collect_item_songs(BrowserScreen *, NcmSongArray *, NcmMpdItem *);
+static bool browser_supported_extensions_contains(StrBuilderArray *, char *, int32 );
 static int32 browser_set_parent_of_directory(BrowserScreen *, char *, int32 );
 static int32 browser_load_mpd_items(BrowserScreen *, NcmMpdItemArray *);
 static int32 browser_reload_from_local(BrowserScreen *, NcmError *);
 static int32 browser_stat_local_path(char *, int32, NcmFsStat *, NcmError *);
-static bool browser_local_path_has_supported_extension(BrowserScreen *, char *, int32 );
 static int32 browser_make_local_song(NcmSong *, char *, int32, time_t );
-static int32 browser_collect_item_songs(BrowserScreen *, NcmSongArray *, NcmMpdItem *);
-static int32 browser_real_path(BrowserScreen *, NcmStringView, StrBuilder *, NcmError *);
 static int32 browser_delete_path_recursive(char *, int32, NcmError *);
-static bool browser_supported_extensions_contains(StrBuilderArray *, char *, int32 );
 static int32 browser_item_sort_rank(NcmMpdItem *);
 static int32 browser_compare_views(NcmStringView, NcmStringView );
 static int32 browser_compare_times(time_t, time_t );
