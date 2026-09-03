19501c48abff3564c9d071228efce7f47477ff9f
lucas-mior <lucas.mior@tutamail.com>
2026-09-03 04:38:45 -0300

    style (whitespace)

--- src/c/ncm_conversion.c
+++ src/c/ncm_conversion.c
@@ -6,8 +6,8 @@
 #include "c/ncm_c.h"
 
 static int32
-ncm_conversion_copy_source(StrBuilder *buffer, char *source,
-                           int32 source_len, NcmError *ncm_error) {
+ncm_conversion_copy_source(StrBuilder *buffer, char *source, int32 source_len,
+                           NcmError *ncm_error) {
     sb_clear(buffer);
 
     if (source == NULL) {
@@ -114,8 +114,7 @@ ncm_parse_int64(char *source, int32 source_len, int32 *out,
                                     STRLIT("missing conversion output"));
     }
 
-    status = ncm_conversion_copy_source(
-        &buffer, source, source_len, ncm_error);
+    status = ncm_conversion_copy_source(&buffer, source, source_len, ncm_error);
     if (status < 0) {
         sb_free(&buffer);
         return status;
@@ -184,8 +183,7 @@ ncm_parse_double(char *source, int32 source_len, double *out,
                                     STRLIT("missing conversion output"));
     }
 
-    status = ncm_conversion_copy_source(
-        &buffer, source, source_len, ncm_error);
+    status = ncm_conversion_copy_source(&buffer, source, source_len, ncm_error);
     if (status < 0) {
         sb_free(&buffer);
         return status;
