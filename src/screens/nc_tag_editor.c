79dd169aba97969f2ea68c9943351d94895c0b52
lucas-mior <lucas.mior@tutamail.com>
2026-09-03 16:56:22 -0300

    organize

--- src/screens/nc_tag_editor.c
+++ src/screens/nc_tag_editor.c
@@ -145,16 +145,12 @@ tag_editor_draw_tag(NcMenu *menu, NcWindow *window, void *item,
     }
 
     {
-        NcBufferProperty *properties;
-        char *data;
-        int32 property_count;
-        int32 property_index;
-        int32 len;
-        data = nc_buffer_data(&buffer);
-        len = nc_buffer_len(&buffer);
-        properties = nc_buffer_properties(&buffer);
-        property_count = nc_buffer_property_count(&buffer);
-        property_index = 0;
+        NcBufferProperty *properties = nc_buffer_properties(&buffer);
+        char *data = nc_buffer_data(&buffer);
+        int32 len = nc_buffer_len(&buffer);
+        int32 property_count = nc_buffer_property_count(&buffer);
+        int32 property_index = 0;
+
         for (int32 i = 0; ; i += 1) {
             while ((property_index < property_count)
                    && (properties[property_index].position == i)) {
