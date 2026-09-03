378cf9f9f1d5b0d24379e0fda177fc41403e1f78
lucas-mior <lucas.mior@tutamail.com>
2026-09-03 14:53:51 -0300

    order of functions

--- src/screens/nc_media_library.c
+++ src/screens/nc_media_library.c
@@ -28,7 +28,6 @@ static void library_update(NcScreen *screen);
 static void library_destroy_callback(NcScreen *screen);
 
 // declarations to delete
-static int32 library_update_tags(MediaLibraryScreen *screen, NcmError *ncm_error);
 static void library_album_array_item_init(void *item);
 static int32 library_update_albums(MediaLibraryScreen *screen, NcmError *ncm_error);
 static void library_restart_update_timer(MediaLibraryScreen *screen);
@@ -2833,40 +2832,6 @@ library_has_fetch_delay_elapsed(MediaLibraryScreen *screen) {
            > screen->fetching_delay_ms;
 }
 
-int32
-media_library_screen_update(MediaLibraryScreen *screen,
-                            NcmError *ncm_error) {
-    if (screen == NULL) {
-        return ncm_error_set_status(ncm_error, -EINVAL,
-                                    STRLIT("missing media library"));
-    }
-    ncm_error_clear(ncm_error);
-
-    if (library_has_pending_tags(screen)) {
-        return library_update_tags(screen, ncm_error);
-    }
-
-    if (library_has_pending_albums(screen)
-        && ((screen->mode != MEDIA_LIBRARY_MODE_THREE_COLUMNS)
-            || screen->albums_update_request
-            || library_has_fetch_delay_elapsed(screen))) {
-        return library_update_albums(screen, ncm_error);
-    }
-
-    if (library_has_pending_songs(screen)
-        && (screen->songs_update_request
-            || library_has_fetch_delay_elapsed(screen))) {
-        return library_update_songs(screen, ncm_error);
-    }
-
-    if (!library_has_pending_tags(screen)
-        && !library_has_pending_albums(screen)
-        && !library_has_pending_songs(screen)) {
-        nc_screen_clear_update_request(&screen->screen);
-    }
-    return 0;
-}
-
 static void
 library_restore_tag_identity(
     NcMediaLibraryTagMenu *menu, NcMediaLibraryTagRow *identity,
@@ -2999,6 +2964,40 @@ library_update_tags(MediaLibraryScreen *screen,
     ncm_mpd_string_list_destroy(&strings);
     media_library_tag_array_destroy(&tags);
     return status;
+}
+
+int32
+media_library_screen_update(MediaLibraryScreen *screen,
+                            NcmError *ncm_error) {
+    if (screen == NULL) {
+        return ncm_error_set_status(ncm_error, -EINVAL,
+                                    STRLIT("missing media library"));
+    }
+    ncm_error_clear(ncm_error);
+
+    if (library_has_pending_tags(screen)) {
+        return library_update_tags(screen, ncm_error);
+    }
+
+    if (library_has_pending_albums(screen)
+        && ((screen->mode != MEDIA_LIBRARY_MODE_THREE_COLUMNS)
+            || screen->albums_update_request
+            || library_has_fetch_delay_elapsed(screen))) {
+        return library_update_albums(screen, ncm_error);
+    }
+
+    if (library_has_pending_songs(screen)
+        && (screen->songs_update_request
+            || library_has_fetch_delay_elapsed(screen))) {
+        return library_update_songs(screen, ncm_error);
+    }
+
+    if (!library_has_pending_tags(screen)
+        && !library_has_pending_albums(screen)
+        && !library_has_pending_songs(screen)) {
+        nc_screen_clear_update_request(&screen->screen);
+    }
+    return 0;
 }
 
 static void
