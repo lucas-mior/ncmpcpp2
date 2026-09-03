8bb090147cf0bb06a10ba84944f725b1a65eadae
lucas-mior <lucas.mior@tutamail.com>
2026-09-03 02:51:42 -0300

    style (whitespace)

--- src/screens/nc_tag_editor.c
+++ src/screens/nc_tag_editor.c
@@ -4864,10 +4864,8 @@ tag_editor_build_parser_preview(TagEditorScreen *screen,
         if (screen->parser_mode
             == TAG_EDITOR_PARSER_TAGS_FROM_FILENAME) {
             if (!apply && song->name) {
-                SB_APPEND(&screen->parser_preview, song->name,
-                          song->name_len);
-                SB_APPEND(&screen->parser_preview,
-                          ":\n");
+                SB_APPEND(&screen->parser_preview, song->name, song->name_len);
+                SB_APPEND(&screen->parser_preview, ":\n");
             }
             status = tag_editor_parse_filename(
                 song, screen->pattern.data, screen->pattern.len, !apply,
