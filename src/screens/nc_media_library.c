e6a8500fbd83c37f4bba74dac2620af59e34e466
lucas-mior <lucas.mior@tutamail.com>
2026-09-03 15:09:23 -0300

    style (whitespace)

--- src/screens/nc_media_library.c
+++ src/screens/nc_media_library.c
@@ -2011,9 +2011,7 @@ media_library_screen_can_move_to_previous_column(
 }
 
 bool
-media_library_screen_can_move_to_next_column(
-    MediaLibraryScreen *screen
-) {
+media_library_screen_can_move_to_next_column(MediaLibraryScreen *screen) {
     if (screen == NULL) {
         return false;
     }
