60dcf5244b82d936af78048932ceeb4712413bc5
lucas-mior <lucas.mior@tutamail.com>
2026-09-03 17:42:26 -0300

    style (whitespace)

--- src/screens/nc_playlist_editor.c
+++ src/screens/nc_playlist_editor.c
@@ -1184,9 +1184,8 @@ playlist_editor_screen_current_playlist(
 }
 
 int32
-playlist_editor_screen_current_song(
-    PlaylistEditorScreen *screen, NcmSong *song
-) {
+playlist_editor_screen_current_song(PlaylistEditorScreen *screen,
+                                    NcmSong *song) {
     return playlist_editor_screen_current_content_song(screen, song);
 }
 
