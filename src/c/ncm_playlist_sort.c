1a0750519c33f871659b5447ad5d138841fb2fb0
lucas-mior <lucas.mior@tutamail.com>
2026-09-03 03:23:43 -0300

    style (whitespace)

--- src/c/ncm_playlist_sort.c
+++ src/c/ncm_playlist_sort.c
@@ -197,8 +197,7 @@ ncm_playlist_sort_plan_build(
     order = malloc2(songs->len*SIZEOF(*order));
     temporary = malloc2(songs->len*SIZEOF(*temporary));
     current = malloc2(songs->len*SIZEOF(*current));
-    replacement.items = malloc2(
-        (songs->len - 1)*SIZEOF(*replacement.items));
+    replacement.items = malloc2((songs->len - 1)*SIZEOF(*replacement.items));
     replacement.cap = songs->len - 1;
 
     for (int32 i = 0; i < songs->len; i += 1) {
@@ -279,8 +278,9 @@ ncm_playlist_sort_plan_execute(NcmPlaylistSortPlan *plan,
         started = true;
     }
     for (int32 i = 0; (status == 0) && (i < plan->len); i += 1) {
-        status = ncm_mpd_client_swap(client, plan->items[i].from,
-                                     plan->items[i].to, ncm_error);
+        status = ncm_mpd_client_swap(client,
+                                     plan->items[i].from, plan->items[i].to,
+                                     ncm_error);
     }
     if (status == 0) {
         status = ncm_mpd_client_commit_command_list(client, ncm_error);
