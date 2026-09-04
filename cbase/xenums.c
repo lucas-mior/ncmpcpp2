5d99588985e166d3343a9c543aa4ee6125cf03a7
lucas-mior <lucas.mior@tutamail.com>
2026-09-04 14:10:41 -0300

    style (whitespace)

--- cbase/xenums.c
+++ cbase/xenums.c
@@ -163,10 +163,10 @@ XENUMS_LINKAGE int32
 CAT(ENUM_PREFIX_, str_len)(enum ENUM_NAME val, char **out) {
 #if ENUM_BITFLAGS == 0
     switch (val) {
-        #define XENUM_STR_1(e)    case e:                                       \
+        #define XENUM_STR_1(e)    case e:                                      \
                                      *out = #e;                                \
                                      return STRLIT_LEN(#e);
-        #define XENUM_STR_2(e, v) case e:                                       \
+        #define XENUM_STR_2(e, v) case e:                                      \
                                      *out = #e;                                \
                                      return STRLIT_LEN(#e);
         #define XX(...)          SELECT_ON_NUM_ARGS(XENUM_STR_, __VA_ARGS__)
@@ -267,10 +267,10 @@ XENUMS_LINKAGE int32
 CAT(ENUM_PREFIX_, alias_len)(enum ENUM_NAME val, char **out) {
 #if ENUM_BITFLAGS == 0
     switch (val) {
-        #define XENUM_ALIAS_1(e)        case e:                            \
+        #define XENUM_ALIAS_1(e)        case e:                                \
                                                *out = #e;                      \
                                                return STRLIT_LEN(#e);
-        #define XENUM_ALIAS_2(e, alias) case e:                            \
+        #define XENUM_ALIAS_2(e, alias) case e:                                \
                                                *out = #alias;                  \
                                                return STRLIT_LEN(#alias);
         #define XX(...) SELECT_ON_NUM_ARGS(XENUM_ALIAS_, __VA_ARGS__)
