18dbfbf9ba38cbae9a133fd49ab2390e1dc7324c
lucas-mior <lucas.mior@tutamail.com>
2026-09-03 21:08:50 -0300

    wip

--- src/screens/nc_tag_editor.c
+++ src/screens/nc_tag_editor.c
@@ -356,20 +356,12 @@ tag_editor_focus_is_parser_helper(enum TagEditorFocus focus) {
 
 static void
 tag_editor_update_menu_highlights(TagEditorScreen *screen) {
-    NcMenu *directories;
-    NcMenu *tag_types;
-    NcMenu *tags;
-    NcMenu *parser_dialog;
-    NcMenu *parser_rows;
-    NcMenu *parser_actions;
-    NcMenu *active;
-
-    directories = nc_editor_pair_menu_base(&screen->directories);
-    tag_types = nc_editor_string_menu_base(&screen->tag_types);
-    tags = nc_tag_row_menu_base(&screen->tags);
-    parser_dialog = nc_editor_string_menu_base(&screen->parser_dialog);
-    parser_rows = nc_editor_string_menu_base(&screen->parser_rows);
-    parser_actions = nc_editor_string_menu_base(&screen->parser_actions);
+    NcMenu *directories = nc_editor_pair_menu_base(&screen->directories);
+    NcMenu *tag_types = nc_editor_string_menu_base(&screen->tag_types);
+    NcMenu *tags = nc_tag_row_menu_base(&screen->tags);
+    NcMenu *parser_dialog = nc_editor_string_menu_base(&screen->parser_dialog);
+    NcMenu *parser_rows = nc_editor_string_menu_base(&screen->parser_rows);
+    NcMenu *parser_actions = nc_editor_string_menu_base(&screen->parser_actions);
 
     nc_menu_set_highlight_prefix(directories,
                                  &Config.current_item_inactive_column_prefix);
@@ -396,9 +388,12 @@ tag_editor_update_menu_highlights(TagEditorScreen *screen) {
     nc_menu_set_highlight_suffix(parser_actions,
                                  &Config.current_item_inactive_column_suffix);
 
-    if ((active = tag_editor_screen_active_menu(screen))) {
-        nc_menu_set_highlight_prefix(active, &Config.current_item_prefix);
-        nc_menu_set_highlight_suffix(active, &Config.current_item_suffix);
+    {
+        NcMenu *active;
+        if ((active = tag_editor_screen_active_menu(screen))) {
+            nc_menu_set_highlight_prefix(active, &Config.current_item_prefix);
+            nc_menu_set_highlight_suffix(active, &Config.current_item_suffix);
+        }
     }
     {
         NcBorder dialog_border = Config.window_border;
