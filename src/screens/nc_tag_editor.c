31a258f43c7d18033365f6a10738f8fee28b2e55
lucas-mior <lucas.mior@tutamail.com>
2026-09-03 21:05:12 -0300

    style (whitespace)

--- src/screens/nc_tag_editor.c
+++ src/screens/nc_tag_editor.c
@@ -3361,9 +3361,9 @@ tag_editor_run_current(NcScreen *screen) {
             if (editor->hooks.prompt == NULL) {
                 prompt_result = TAG_EDITOR_PROMPT_ERROR;
             } else {
-                prompt_result = editor->hooks.prompt(
-                    editor->hooks.user, STRLIT("New filename"), initial,
-                    &input);
+                prompt_result = editor->hooks.prompt(editor->hooks.user,
+                                                     STRLIT("New filename"),
+                                                     initial, &input);
             }
             if (prompt_result == TAG_EDITOR_PROMPT_ABORTED) {
                 tag_editor_status_message(editor, STRLIT("Action aborted"));
