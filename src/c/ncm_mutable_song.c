d4dcee919401cde24f876ebcc3fcb60aa1da0d10
lucas-mior <lucas.mior@tutamail.com>
2026-09-05 01:25:56 -0300

    style (whitespace)

--- src/c/ncm_mutable_song.c
+++ src/c/ncm_mutable_song.c
@@ -631,8 +631,7 @@ ncm_mutable_song_set_new_name(NcmMutableSong *song, char *new_name,
         ncm_mutable_song_free_string(&song->new_name, &song->new_name_len);
         return 0;
     }
-    if (optional_strequal(song->name, song->name_len, new_name,
-                          new_name_len)) {
+    if (optional_strequal(song->name, song->name_len, new_name, new_name_len)) {
         ncm_mutable_song_free_string(&song->new_name, &song->new_name_len);
         return 0;
     }
@@ -643,8 +642,7 @@ ncm_mutable_song_set_new_name(NcmMutableSong *song, char *new_name,
 }
 
 bool
-ncm_mutable_song_has_new_name_view(NcmMutableSong *song,
-                                   NcmStringView *view) {
+ncm_mutable_song_has_new_name_view(NcmMutableSong *song, NcmStringView *view) {
     if (view == NULL) {
         return false;
     }
