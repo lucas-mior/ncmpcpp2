c9b47583bdadc794d1653b66acf078c7249f3136
lucas-mior <lucas.mior@tutamail.com>
2026-09-05 02:37:11 -0300

    style (whitespace)

--- src/screens/app_screens.c
+++ src/screens/app_screens.c
@@ -153,11 +153,11 @@ NCM_APP_SCREEN_DIRECT_ACCESSOR_TYPES(NCM_APP_SCREEN_DIRECT_ACCESSOR)
 
 #undef NCM_APP_SCREEN_DIRECT_ACCESSOR
 
-#define NCM_APP_SCREEN_DEFINE_WRAPPED_ACCESSOR(suffix, base_expr)              \
-    NcScreen *                                                                 \
-    app_screen_##suffix##_base(void) {                                         \
-        app_screen_##suffix##_init();                                          \
-        return base_expr;                                                      \
+#define NCM_APP_SCREEN_DEFINE_WRAPPED_ACCESSOR(suffix, base_expr)           \
+    NcScreen *                                                              \
+    app_screen_##suffix##_base(void) {                                      \
+        app_screen_##suffix##_init();                                       \
+        return base_expr;                                                   \
     }
 
 NCM_APP_SCREEN_WRAPPED_ACCESSOR_TYPES(NCM_APP_SCREEN_DEFINE_WRAPPED_ACCESSOR)
@@ -165,10 +165,10 @@ NCM_APP_SCREEN_WRAPPED_ACCESSOR_TYPES(NCM_APP_SCREEN_DEFINE_WRAPPED_ACCESSOR)
 #undef NCM_APP_SCREEN_DEFINE_WRAPPED_ACCESSOR
 
 #define NCM_APP_SCREEN_TYPED_WRAPPED_ACCESSOR(suffix, function, type, expr) \
-    type *                                                                     \
-    function(void) {                                                           \
-        app_screen_##suffix##_init();                                          \
-        return expr;                                                           \
+    type *                                                                  \
+    function(void) {                                                        \
+        app_screen_##suffix##_init();                                       \
+        return expr;                                                        \
     }
 
 NCM_APP_SCREEN_TYPED_WRAPPED_ACCESSOR_TYPES(NCM_APP_SCREEN_TYPED_WRAPPED_ACCESSOR)
