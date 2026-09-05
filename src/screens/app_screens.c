3edaf5926594fd4dffc9e3d11c1793f4d56d4e38
lucas-mior <lucas.mior@tutamail.com>
2026-09-05 02:36:58 -0300

    wip

--- src/screens/app_screens.c
+++ src/screens/app_screens.c
@@ -164,17 +164,16 @@ NCM_APP_SCREEN_WRAPPED_ACCESSOR_TYPES(NCM_APP_SCREEN_DEFINE_WRAPPED_ACCESSOR)
 
 #undef NCM_APP_SCREEN_DEFINE_WRAPPED_ACCESSOR
 
-#define NCM_APP_SCREEN_DEFINE_TYPED_WRAPPED_ACCESSOR(suffix, function, type, expr) \
+#define NCM_APP_SCREEN_TYPED_WRAPPED_ACCESSOR(suffix, function, type, expr) \
     type *                                                                     \
     function(void) {                                                           \
         app_screen_##suffix##_init();                                          \
         return expr;                                                           \
     }
 
-NCM_APP_SCREEN_TYPED_WRAPPED_ACCESSOR_TYPES(
-    NCM_APP_SCREEN_DEFINE_TYPED_WRAPPED_ACCESSOR)
+NCM_APP_SCREEN_TYPED_WRAPPED_ACCESSOR_TYPES(NCM_APP_SCREEN_TYPED_WRAPPED_ACCESSOR)
 
-#undef NCM_APP_SCREEN_DEFINE_TYPED_WRAPPED_ACCESSOR
+#undef NCM_APP_SCREEN_TYPED_WRAPPED_ACCESSOR
 
 #define NCM_APP_SCREEN_DEFINE_STANDARD_REGISTER(suffix)                        \
     void                                                                       \
