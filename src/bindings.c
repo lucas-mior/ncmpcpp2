9f63077fe76aef041421ec25e23ef762bc37c130
lucas-mior <lucas.mior@tutamail.com>
2026-09-05 02:19:42 -0300

    style (whitespace)

--- src/bindings.c
+++ src/bindings.c
@@ -270,8 +270,7 @@ ncm_binding_runtime_run_external_console_command(char *command,
 
     (void)user;
     nc_pause_screen();
-    status = ncm_macro_run_external_console_command(command, command_len,
-                                                    NULL);
+    status = ncm_macro_run_external_console_command(command, command_len, NULL);
     nc_unpause_screen();
     return status;
 }
@@ -286,8 +285,7 @@ ncm_binding_default_runtime(void) {
         runtime.run_action = ncm_binding_runtime_run_action;
         runtime.current_screen_is = ncm_binding_runtime_current_screen_is;
         runtime.push_key = ncm_binding_runtime_push_key;
-        runtime.run_external_command =
-            ncm_binding_runtime_run_external_command;
+        runtime.run_external_command = ncm_binding_runtime_run_external_command;
         runtime.run_external_console_command =
             ncm_binding_runtime_run_external_console_command;
         runtime.user = ncm_action_runtime_global();
