cb44532f3b3c710826356a99c1177bde00665d62
lucas-mior <lucas.mior@tutamail.com>
2026-09-07 20:23:41 -0300

    style (whitespace)

--- src/screens/nc_visualizer.c
+++ src/screens/nc_visualizer.c
@@ -317,7 +317,7 @@ visualizer_update_callback(NcScreen *screen) {
             visualizer->incoming_samples.data, buffer_size);
         if (bytes_read > 0) {
             samples_read = (int32)(bytes_read
-                /SIZEOF(*visualizer->incoming_samples.data));
+                                   /SIZEOF(*visualizer->incoming_samples.data));
             if (samples_read > 0) {
                 visualizer_screen_push_samples(
                     visualizer, visualizer->incoming_samples.data,
@@ -357,6 +357,7 @@ visualizer_update_callback(NcScreen *screen) {
 #define NC_SCREEN_IMPL_LOCKABLE true
 #define NC_SCREEN_IMPL_MERGABLE true
 #include "screens/nc_screen_impl_template.h"
+
 static int32
 visualizer_system_open_fifo(void *user, char *location, int32 location_len) {
     int32 error_code;
