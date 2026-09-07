a6aa07a05ce5a51f6154ab4f3c8c9df634c6071c
lucas-mior <lucas.mior@tutamail.com>
2026-09-07 20:31:44 -0300

    style (whitespace)

--- src/screens/nc_visualizer.c
+++ src/screens/nc_visualizer.c
@@ -1297,8 +1297,7 @@ static void
 visualizer_draw_wave(VisualizerScreen *screen,
                      int16 *samples, int32 samples_len,
                      int32 y_offset, int32 height) {
-    char *character = screen->visualizer_chars.data
-                      + screen->point_char_offset;
+    char *character = screen->visualizer_chars.data + screen->point_char_offset;
     int32 character_len = screen->point_char_len;
     int32 width = nc_window_width(&screen->window);
     int32 half_height;
