aa0b190364f09390e0e07fb3e32f093dd031ccc1
lucas-mior <lucas.mior@tutamail.com>
2026-09-07 20:16:29 -0300

    style (whitespace)

--- src/screens/nc_visualizer.c
+++ src/screens/nc_visualizer.c
@@ -1617,14 +1617,10 @@ visualizer_draw_frequency(VisualizerScreen *screen,
                     double h01;
                     double h11;
 
-                    x_previous2 =
-                        fft->bar_heights[height_index - 2].column;
-                    h_previous2 =
-                        fft->bar_heights[height_index - 2].height;
-                    x_previous =
-                        fft->bar_heights[height_index - 1].column;
-                    h_previous =
-                        fft->bar_heights[height_index - 1].height;
+                    x_previous2 = fft->bar_heights[height_index - 2].column;
+                    h_previous2 = fft->bar_heights[height_index - 2].height;
+                    x_previous = fft->bar_heights[height_index - 1].column;
+                    h_previous = fft->bar_heights[height_index - 1].height;
                     x_next2 = fft->bar_heights[height_index + 1].column;
                     h_next2 = fft->bar_heights[height_index + 1].height;
                     if ((x_previous == x_previous2) || (x_next2 == x_next)
