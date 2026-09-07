cd4a1c918116083f1666a77e86786b16739697ba
lucas-mior <lucas.mior@tutamail.com>
2026-09-07 20:18:07 -0300

    style (whitespace)

--- src/screens/nc_visualizer.c
+++ src/screens/nc_visualizer.c
@@ -1625,11 +1625,9 @@ visualizer_draw_frequency(VisualizerScreen *screen,
                         || (x_next == x_prev)) {
                         h = h_next;
                     } else {
-                        m0 = (h_prev - h_prev2)
-                             /(x_prev - x_prev2);
+                        m0 = (h_prev - h_prev2) /(x_prev - x_prev2);
                         m1 = (h_next2 - h_next)/(x_next2 - x_next);
-                        t = ((double)x - x_prev)
-                            /(x_next - x_prev);
+                        t = ((double)x - x_prev) /(x_next - x_prev);
                         h00 = 2.0*t*t*t - 3.0*t*t + 1.0;
                         h10 = t*t*t - 2.0*t*t + t;
                         h01 = -2.0*t*t*t + 3.0*t*t;
