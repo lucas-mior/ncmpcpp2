2c3480dbf98b507a518952d8f67de0339255aff7
lucas-mior <lucas.mior@tutamail.com>
2026-09-08 22:07:38 -0300

    wip

--- src/screens/nc_media_library.c
+++ src/screens/nc_media_library.c
@@ -2502,11 +2502,12 @@ library_apply_column_filter(MediaLibraryScreen *screen,
     NcMenuDisplayCallbacks callbacks;
 
     state = media_library_screen_column_state(screen, column);
+    callbacks = library_display_callbacks(screen, column,
+                                          state->filter_enabled);
+
     ASSERT(state != NULL);
     ASSERT(menu != NULL);
 
-    callbacks = library_display_callbacks(screen, column,
-                                          state->filter_enabled);
     nc_menu_set_display_callbacks(menu, callbacks);
     if (state->filter_enabled) {
         nc_menu_apply_filter(menu);
