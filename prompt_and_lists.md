504c70fa784f43d72b318560d1aa4847f818a59e
lucas-mior <lucas.mior@tutamail.com>
2026-09-09 14:42:33 -0300

    cleanup

--- prompt_and_lists.md
+++ prompt_and_lists.md
@@ -36,9 +36,6 @@ int x = function_with_long_name_must_be_broken_twice(long_argument,
                                                      window_height);
 ```
 
-- src/app_controller.c
-- src/screens/nc_scrollpad_screen.c
-- src/c/ncm_comparators.c
 - src/screens/nc_tiny_tag_edit.c
 - src/main.c
 - src/c/ncm_taglib.c
--- src/screens/nc_scrollpad_screen.c
+++ src/screens/nc_scrollpad_screen.c
@@ -8,8 +8,9 @@
 void
 nc_scrollpad_screen_init(NcScrollpadScreen *screen,
                          NcScreenOps callbacks, void *user,
-                         enum NcScreenType type, int32 start_x,
-                         int32 start_y, int32 width, int32 height) {
+                         enum NcScreenType type,
+                         int32 start_x, int32 start_y,
+                         int32 width, int32 height) {
     nc_screen_init_ops(&screen->base, callbacks, user, type);
     nc_scrollpad_screen_set_geometry(screen, start_x, start_y, width, height);
     return;
