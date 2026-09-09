7cb81082faa8360068d4744045e016bd95970f13
lucas-mior <lucas.mior@tutamail.com>
2026-09-08 22:41:21 -0300

    cleanup

--- src/screens/nc_playlist_edit.c
+++ src/screens/nc_playlist_edit.c
@@ -323,7 +323,6 @@ playlist_edit_update_callback(NcScreen *screen) {
     PlaylistEditScreen *editor;
     NcmError ncm_error = {0};
     NcMenu *playlists;
-    NcMenu *content;
     bool content_fetch_due;
     bool displayed_playlist_is_current;
     int32 changed;
@@ -354,7 +353,7 @@ playlist_edit_update_callback(NcScreen *screen) {
     playlist_edit_finish_playlist_change(editor);
     content_fetch_due = false;
     if (nc_menu_item_count(playlists) > 0) {
-        content = nc_song_menu_base(&editor->content);
+        NcMenu *content = nc_song_menu_base(&editor->content);
         displayed_playlist_is_current =
             playlist_edit_displayed_playlist_is_current(editor);
         if (editor->content_update_requested && displayed_playlist_is_current) {
