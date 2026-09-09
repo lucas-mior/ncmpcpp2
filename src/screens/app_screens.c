a4e5e6750c4962964653d991c307bed8a27b5d59
lucas-mior <lucas.mior@tutamail.com>
2026-09-09 11:50:53 -0300

    fix search

--- src/screens/app_screens.c
+++ src/screens/app_screens.c
@@ -658,12 +658,7 @@ app_screen_search_engine_init(void) {
                               ui_state_main_height(), Config.main_window_color,
                               no_border());
 
-    mode = SEARCH_ENGINE_SEARCH_MODE_LITERAL;
-    if (Config.search_engine_default_search_mode
-        < (int32)SEARCH_ENGINE_SEARCH_MODE_COUNT) {
-        mode = (enum SearchEngineSearchMode)
-            Config.search_engine_default_search_mode;
-    }
+    mode = configuration_search_engine_default_mode(&Config);
     search_engine_screen_set_search_mode(&search_engine_screen, mode);
     search_engine_screen_set_search_source(&search_engine_screen,
                                            Config.default_place_to_search_in);
