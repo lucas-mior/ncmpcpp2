e8dab41bf9a982b2a888a9d4ec1c52d83543b7d1
lucas-mior <lucas.mior@tutamail.com>
2026-09-09 11:23:43 -0300

    style (whitespace)

--- src/actions.c
+++ src/actions.c
@@ -138,7 +138,7 @@ XX(toggle_browser_sort_mode, TOGGLE_BROWSER_SORT_MODE)                         \
 XX(toggle_consume, TOGGLE_CONSUME)                                             \
 XX(toggle_crossfade, TOGGLE_CROSSFADE)                                         \
 XX(toggle_display_mode, TOGGLE_DISPLAY_MODE)                                   \
-XX(toggle_fetch_lyrics_in_background, TOGGLE_FETCHING_LYRICS_IN_BACKGROUND) \
+XX(toggle_fetch_lyrics_in_background, TOGGLE_FETCHING_LYRICS_IN_BACKGROUND)    \
 XX(toggle_find_mode, TOGGLE_FIND_MODE)                                         \
 XX(toggle_interface, TOGGLE_INTERFACE)                                         \
 XX(toggle_library_tag_type, TOGGLE_LIBRARY_TAG_TYPE)                           \
