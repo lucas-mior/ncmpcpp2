b16ef367ffe8cf65aab1c5bc890b72d367b293b7
lucas-mior <lucas.mior@tutamail.com>
2026-09-09 16:24:04 -0300

    cleanup

--- src/screens/nc_playlist_edit.c
+++ src/screens/nc_playlist_edit.c
@@ -313,8 +313,7 @@ playlist_edit_report_error(char *context, int32 context_len,
         SB_APPEND(&message, ": ");
         SB_APPEND(&message, ncm_error->message, strlen32(ncm_error->message));
     }
-    ncm_statusbar_print(Config.message_delay_time,
-                        message.data, strlen32(message.data));
+    ncm_statusbar_print(Config.message_delay_time, message.data, message.len);
     sb_free(&message);
     return;
 }
