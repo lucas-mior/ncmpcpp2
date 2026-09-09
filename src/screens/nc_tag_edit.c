d3b81f4373e695f222d6e0cb4741ffdaba97b6b1
lucas-mior <lucas.mior@tutamail.com>
2026-09-08 21:48:12 -0300

    style (whitespace)

--- src/screens/nc_tag_edit.c
+++ src/screens/nc_tag_edit.c
@@ -1902,8 +1902,8 @@ tag_edit_directory_filter(NcMenu *menu, void *item, void *user) {
         return true;
     }
     return tag_edit_directory_matches_regex(pair,
-                                              &screen->directory_filter_regex,
-                                              true);
+                                            &screen->directory_filter_regex,
+                                            true);
 }
 
 static NcMenuDisplayCallbacks
