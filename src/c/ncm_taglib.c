d807ed62429c4b68641ae2609efc717dc450c28f
lucas-mior <lucas.mior@tutamail.com>
2026-09-10 18:21:17 -0300

    wip

--- src/c/ncm_taglib.c
+++ src/c/ncm_taglib.c
@@ -21,10 +21,10 @@ typedef struct NcmTaglibPropertyMap {
 static bool ncm_taglib_is_initialized;
 
 static NcmTaglibPropertyMap ncm_taglib_properties[] = {
-#define NCM_TAGLIB_PROPERTY_MAP(tag, display, tag_char, field, getter,        \
-                                getter_char, flags)                     \
+#define NCM_TAGLIB_PROPERTY_MAP(tag_id, display, tag_char, field, getter,     \
+                                getter_char, flags)                         \
     {                                                                        \
-        .tag = tag,                                                          \
+        .tag = tag_id,                                                       \
     },
 
     NCM_TAGLIB_TAG_DEFS(NCM_TAGLIB_PROPERTY_MAP)
