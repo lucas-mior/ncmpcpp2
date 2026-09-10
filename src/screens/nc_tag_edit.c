c6c3664d6690d1ffea88109ad66773e1d7d4ed6a
lucas-mior <lucas.mior@tutamail.com>
2026-09-10 20:23:11 -0300

    cleanup

--- src/screens/nc_tag_edit.c
+++ src/screens/nc_tag_edit.c
@@ -778,7 +778,7 @@ tag_edit_build_parser_legend(TagEditScreen *screen) {
 
     sb_clear(&screen->parser_legend);
 
-    TAG_EDIT_PARSER_DEFS(TAG_EDIT_APPEND_PARSER_FIELD)
+    TAG_DEFAULT_ORDER_DEFS(TAG_EDIT_APPEND_PARSER_FIELD)
     SB_APPEND(&screen->parser_legend, "\nFiles:\n");
 
     tags = nc_tag_row_menu_base(&screen->tags);
