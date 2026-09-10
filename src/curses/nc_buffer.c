b04b66e8242073965be81efa8177b2a52c72693f
lucas-mior <lucas.mior@tutamail.com>
2026-09-10 01:18:35 -0300

    fix nc_buffer_append_int64

--- src/curses/nc_buffer.c
+++ src/curses/nc_buffer.c
@@ -173,7 +173,7 @@ nc_buffer_append_char(NcBuffer *buffer, char ch) {
 void
 nc_buffer_append_int64(NcBuffer *buffer, int64 value) {
     nc_buffer_reserve(buffer, 22);
-    buffer->len += itoa2(buffer->data, 22, value);
+    buffer->len += itoa2(buffer->data + buffer->len, 22, value);
     return;
 }
 
