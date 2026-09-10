edece06b0346067499ba943a53a53b7629f59fe8
lucas-mior <lucas.mior@tutamail.com>
2026-09-10 20:08:11 -0300

    wip

--- src/ncmpcpp2.h
+++ src/ncmpcpp2.h
@@ -520,22 +520,23 @@ ncm_tag_type_taglib_property_len(enum TagType tag, char *out, int32 cap) {
     ASSERT_POSITIVE(cap);
 
     switch ((int32)tag) {
-#define TAGLIB_PROPERTY_CASE(SUFFIX, DISP, CHAR, GETTER_CHAR, FLAGS)   \
-    case CAT(TAG_, SUFFIX):                                            \
-        if (TAG_DISPLAY_NAME_LEN(DISP) >= cap) {                       \
-            out[0] = '\0';                                             \
-            return -1;                                                 \
-        }                                                              \
-        result = ascii_normalize_upper_compact(                        \
-            out, TAG_DISPLAY_NAME(DISP), TAG_DISPLAY_NAME_LEN(DISP));  \
-        if (((FLAGS) & TAG_FLAG_TAGLIB_NUMBER) == 0) {                 \
-            return result;                                             \
-        }                                                              \
-        if (result + STRLIT_LEN("NUMBER") >= cap) {                    \
-            out[0] = '\0';                                             \
-            return -1;                                                 \
-        }                                                              \
-        memcpy64(out + result, STRLIT("NUMBER") + 1);                  \
+#define TAGLIB_PROPERTY_CASE(SUFFIX, DISP, CHAR, GETTER_CHAR, FLAGS)        \
+    case CAT(TAG_, SUFFIX):                                                 \
+        if (TAG_DISPLAY_NAME_LEN(DISP) >= cap) {                            \
+            out[0] = '\0';                                                  \
+            return -1;                                                      \
+        }                                                                   \
+        result = ascii_normalize_upper_compact(out,                         \
+                                               TAG_DISPLAY_NAME(DISP),      \
+                                               TAG_DISPLAY_NAME_LEN(DISP)); \
+        if (((FLAGS) & TAG_FLAG_TAGLIB_NUMBER) == 0) {                      \
+            return result;                                                  \
+        }                                                                   \
+        if (result + STRLIT_LEN("NUMBER") >= cap) {                         \
+            out[0] = '\0';                                                  \
+            return -1;                                                      \
+        }                                                                   \
+        memcpy64(out + result, STRLIT("NUMBER") + 1);                       \
         return result + STRLIT_LEN("NUMBER");
 
     TAGLIB_TAG_DEFS(TAGLIB_PROPERTY_CASE)
