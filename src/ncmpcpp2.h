0c0ccd5e4a678eede257623990594586bbe0146b
lucas-mior <lucas.mior@tutamail.com>
2026-09-10 20:00:58 -0300

    wip

--- src/ncmpcpp2.h
+++ src/ncmpcpp2.h
@@ -43,11 +43,11 @@
 #define TAG_NON_DISP(XX, suffix, DISP)                             \
   XX(suffix, DISP, '\0', '\0', TAG_FLAGS_NONE)
 
-#define TAG_FIELD_MPD_NUM_DECLS(XX)                                \
-  TAG_FIELD_MPD_NUM(XX, TRACK, Track, 'n', 'N')                    \
+#define TAG_FIELD_MPD_NUM_DECLS(XX)                                            \
+  TAG_FIELD_MPD_NUM(XX, TRACK, Track, 'n', 'N')                                \
   TAG_FIELD_MPD_NUM(XX, DISC, Disc, 'd', 'd')
 
-#define TAG_SEARCH_MPD_DECLS(XX)                                   \
+#define TAG_SEARCH_MPD_DECLS(XX)                                               \
   TAG_SEARCH_MPD(XX, NAME, Filename)
 
 #define TAG_EXTENDED_DECLS(XX)                                                 \
