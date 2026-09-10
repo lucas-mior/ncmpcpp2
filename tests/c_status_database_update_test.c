6d348b7d98901363f7d8acb1df7324c99a2ecc15
lucas-mior <lucas.mior@tutamail.com>
2026-09-10 01:35:45 -0300

    fix seek printing database update

---
+++ tests/c_status_database_update_test.c
@@ -0,0 +1,30 @@
+#define main ncmpcpp2_application_main
+#include "main.c"
+#undef main
+
+static void
+test_database_update_state_is_quiet_without_change(void) {
+    status_db_updating = 0;
+
+    ASSERT(!status_database_update_state_changed(0));
+    ASSERT_ZERO(status_db_updating);
+
+    ASSERT(status_database_update_state_changed(12));
+    ASSERT(status_db_updating == 'U');
+
+    ASSERT(!status_database_update_state_changed(13));
+    ASSERT(status_db_updating == 'U');
+
+    ASSERT(status_database_update_state_changed(0));
+    ASSERT_ZERO(status_db_updating);
+
+    ASSERT(!status_database_update_state_changed(0));
+    ASSERT_ZERO(status_db_updating);
+    return;
+}
+
+int
+main(void) {
+    test_database_update_state_is_quiet_without_change();
+    return 0;
+}
