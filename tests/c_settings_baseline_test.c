276916adc39ca4f57e6fb0d091944237632c6cbd
lucas-mior <lucas.mior@tutamail.com>
2026-09-12 14:12:09 -0300

    cleanup

--- tests/c_settings_baseline_test.c
+++ tests/c_settings_baseline_test.c
@@ -1306,8 +1306,7 @@ test_tag_edit_parser_metadata(void) {
 
 #define TEST_PARSER_FIELD(suffix, display, tag_char)                           \
     ASSERT(ncm_tag_type_format_char(CAT(TAG_, suffix)) == tag_char);           \
-    name_len = ncm_tag_type_parser_name_len(CAT(TAG_, suffix),                 \
-                                              &name);                          \
+    name_len = ncm_tag_type_parser_name_len(CAT(TAG_, suffix), &name);         \
     ASSERT(name_len > 0);                                                      \
     tag_edit_append_parser_legend_field(&legend,                               \
                                         CAT(TAG_, suffix));                    \
