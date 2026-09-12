304a6d25fd96ed6fc5326d0bce5e4f94e305eacc
lucas-mior <lucas.mior@tutamail.com>
2026-09-12 13:59:30 -0300

    style (whitespace)

--- src/tags.h
+++ src/tags.h
@@ -53,7 +53,7 @@ enum {
     XX(SONG_GETTER_URI,       URI, 'F')                               \
     SONG_TAG_DEFS(SONG_TAG_AS_GETTER, SONG_TAG_SPECIAL_AS_GETTER, XX) \
     XX(SONG_GETTER_TRACK_TOTAL, Total Tracks, 'N')                    \
-    XX(SONG_GETTER_PRIORITY, Priority, 'P')
+    XX(SONG_GETTER_PRIORITY,    Priority, 'P')
 
 #define SONG_GETTER_ENUM_FIELD(GETTER, DISP, CHAR) \
   XX(GETTER, DISP)
