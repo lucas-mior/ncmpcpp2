ffeab25a753d94c47a0b2e1e5513463dabdc9108
lucas-mior <lucas.mior@tutamail.com>
2026-09-13 14:05:43 -0300

    wip

--- src/c/ncm_array_impl_template.h
+++ src/c/ncm_array_impl_template.h
@@ -23,6 +23,12 @@
 
 #include "cbase.h"
 
+#if defined(__INCLUDE_LEVEL__) && (__INCLUDE_LEVEL__ == 0)
+#define NCM_ARRAY_TYPE      ncm_unused
+#define NCM_ARRAY_ITEM_TYPE int32
+#define NCM_ARRAY_PREFIX    ncm_unused2
+#endif
+
 #if !defined(NCM_ARRAY_TYPE)
 #error "NCM_ARRAY_TYPE must be defined"
 #endif
