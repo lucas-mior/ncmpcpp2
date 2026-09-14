6c14a21ad9f3c56db948170fc018a29539be2268
lucas-mior <lucas.mior@tutamail.com>
2026-09-14 09:10:10 -0300

    style (whitespace)

--- src/screens/nc_lastfm.c
+++ src/screens/nc_lastfm.c
@@ -112,8 +112,9 @@ void
 nc_lastfm_screen_init(NcLastfmScreen *screen, NcScreenOps callbacks, void *user,
                       int32 start_x, int32 width,
                       int32 main_start_y, int32 main_height) {
-    nc_scrollpad_screen_init(&screen->scrollpad_screen, callbacks, user,
-                             NC_SCREEN_TYPE_LASTFM, 0, 0, 0, 0);
+    nc_scrollpad_screen_init(&screen->scrollpad_screen,
+                             callbacks, user, NC_SCREEN_TYPE_LASTFM,
+                             0, 0, 0, 0);
     nc_lastfm_screen_set_geometry(screen,
                                   start_x, width, main_start_y, main_height);
     return;
