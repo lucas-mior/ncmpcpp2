1705bcfb4cbb80901db0223ea139a299c72cef2d
lucas-mior <lucas.mior@tutamail.com>
2026-09-14 19:28:47 -0300

    wip

--- src/bindings.c
+++ src/bindings.c
@@ -790,8 +790,7 @@ bindings_bind_group(BindingsConfiguration *bindings,
   XX_GROUP("c", ACTION_CLEAR_PLAYLIST, ACTION_CLEAR_MAIN_PLAYLIST)             \
   XX_GROUP("C", ACTION_CROP_PLAYLIST, ACTION_CROP_MAIN_PLAYLIST)               \
   XX_GROUP("m", ACTION_MOVE_SORT_ORDER_UP, ACTION_MOVE_SELECTED_ITEMS_UP)      \
-  XX_GROUP("n", ACTION_MOVE_SORT_ORDER_DOWN,                                   \
-           ACTION_MOVE_SELECTED_ITEMS_DOWN)                                    \
+  XX_GROUP("n", ACTION_MOVE_SORT_ORDER_DOWN, ACTION_MOVE_SELECTED_ITEMS_DOWN)  \
   XX_SEQ("M", ACTION_MOVE_SELECTED_ITEMS_TO)                                   \
   XX_SEQ("A", ACTION_ADD)                                                      \
   XX_SEQ("S", ACTION_SAVE_PLAYLIST)                                            \
