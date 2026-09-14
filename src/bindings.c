9bf79f6fd5fd4243dff3b2374162a2ef42f20b44
lucas-mior <lucas.mior@tutamail.com>
2026-09-14 19:38:38 -0300

    style (whitespace)

--- src/bindings.c
+++ src/bindings.c
@@ -968,8 +968,9 @@ binding_parse_action_line(BindingAction *action, char *line, int32 line_len,
                 || (ncm_extract_enclosed(line + name_len,
                                          line_len - name_len,
                                          '"', '"', &argument) < 0)) {
-                bindings_error(ncm_error, "missing quoted argument: '%.*s'",
-                                   line_len, line);
+                bindings_error(ncm_error,
+                               "missing quoted argument: '%.*s'",
+                               line_len, line);
                 return -NCM_ERROR_PARSE;
             }
         }
